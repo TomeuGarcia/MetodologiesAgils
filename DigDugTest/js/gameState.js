95ed75abc37819cf44b364103c69f6f633418a68
Tomeu Garcia Pintó <tomeu.garcia@enti.cat>
2022-10-19 23:15:34 +0200

    started grid-locked movement

--- DigDugTest/js/gameState.js
+++ DigDugTest/js/gameState.js
@@ -1,5 +1,6 @@
 var shapeMask;
 
+
 class gameState extends Phaser.Scene
 {
     constructor()
@@ -24,7 +25,9 @@ class gameState extends Phaser.Scene
 
         this.mask = shapeMask.createGeometryMask().setInvertAlpha(true);
 
-        this.foreground = this.add.tileSprite(0, 0, config.width, config.height, 'foreground').setOrigin(0);
+        this.foreground = this.add.tileSprite(gamePrefs.CELL_SIZE * gamePrefs.NUM_CELL_LEFT_OFFSET, gamePrefs.CELL_SIZE * gamePrefs.NUM_CELL_TOP_OFFSET, 
+                                              gamePrefs.CELL_SIZE * gamePrefs.NUM_CELL_WIDTH, gamePrefs.CELL_SIZE * gamePrefs.NUM_CELL_HEIGHT, 
+                                              'foreground').setOrigin(0);
         this.foreground.setMask(this.mask);
 
         this.player = this.physics.add.sprite(10, 10, 'player').setScale(1);
@@ -32,36 +35,68 @@ class gameState extends Phaser.Scene
         this.addSquareToMask();
 
         this.cursorKeys = this.input.keyboard.createCursorKeys();
+
+        this.grid = new Array(gamePrefs.NUM_CELL_HEIGHT);
+        for (var row = 0; row < gamePrefs.NUM_CELL_HEIGHT; ++row)
+        {
+            this.grid[row] = new Array(gamePrefs.NUM_CELL_WIDTH);
+            for (var col = 0; col < gamePrefs.NUM_CELL_WIDTH; ++col)
+            {
+                this.grid[row][col] = 0;
+            }
+        }
     }
 
     update()
     {
-        if (this.cursorKeys.right.isDown)
+        if (this.cursorKeys.right.isDown && this.canMoveHorizontaly(this.player.y))
         {
             this.player.x += gamePrefs.PLAYER_MOVE_SPEED;
             this.addSquareToMask();
         }
-        else if (this.cursorKeys.left.isDown)
+        else if (this.cursorKeys.left.isDown && this.canMoveHorizontaly(this.player.y))
         {
             this.player.x -= gamePrefs.PLAYER_MOVE_SPEED;
             this.addSquareToMask();
         }
-        else if (this.cursorKeys.up.isDown)
+        else if (this.cursorKeys.up.isDown && this.canMoveVertically(this.player.x))
         {
             this.player.y -= gamePrefs.PLAYER_MOVE_SPEED;
             this.addSquareToMask();
         }
-        else if (this.cursorKeys.down.isDown)
+        else if (this.cursorKeys.down.isDown && this.canMoveVertically(this.player.x))
         {
             this.player.y += gamePrefs.PLAYER_MOVE_SPEED;
             this.addSquareToMask();
         }
+
+        this.pixel2cell(0,0);
     }
 
 
     addSquareToMask()
     {
         shapeMask.fillRect(this.player.x - gamePrefs.HALF_PLAYER_SIDE, this.player.y - gamePrefs.HALF_PLAYER_SIDE, gamePrefs.MASK_SIDE, gamePrefs.MASK_SIDE);
+    }
+
+    canMoveHorizontaly(pixelY)
+    {
+        var y = pixelY % gamePrefs.CELL_SIZE;
+        console.log(y);
+        return y > 5 && y < 11;
+    }
+
+    canMoveVertically(pixelX)
+    {
+        var x = pixelX % gamePrefs.CELL_SIZE;
+        return x > 5 && x < 11;
+    }
+
+    pixel2cell(pixelX, pixelY)
+    {
+        //(gamePrefs.NUM_CELL_LEFT_OFFSET * gamePrefs.CELL_SIZE) + 
+        var x = (pixelX / gamePrefs.CELL_SIZE) % gamePrefs.CELL_SIZE;
+        x > 2 || x < 6
     }
 
 }
--- DigDugTest/js/main.js
+++ DigDugTest/js/main.js
@@ -3,15 +3,20 @@
 
 var gamePrefs =
 {
-    PLAYER_MOVE_SPEED: 2,
+    PLAYER_MOVE_SPEED: 0.8,
     HALF_PLAYER_SIDE: 7,
-    MASK_SIDE: 14
+    MASK_SIDE: 14,
+    CELL_SIZE: 16,
+    NUM_CELL_WIDTH: 12,
+    NUM_CELL_HEIGHT: 12,
+    NUM_CELL_LEFT_OFFSET: 1,
+    NUM_CELL_TOP_OFFSET: 3
 }
 
 var config = 
 {
     type: Phaser.AUTO,
-    width: 192, // window.innerWidth
+    width: 256, // window.innerWidth
     height: 256,  // window.innerHeight
     scene:[gameState], // levels/screens/scenes array
     render:
